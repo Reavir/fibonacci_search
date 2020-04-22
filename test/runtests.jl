7e5ced05716d6239e9dd41a0648fa37847818395
Piotr Babicz <lepnik01@gmail.com>
2020-04-22 18:57:08 +0100

    new files

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using svopt, Test
+
+@testset "fibo" begin
+    a, b = -2, 1
+    f = x -> x^2
+    @test fibo(f, a, b) == (-0.38524590163934436, 0.38524590163934436) 
+    a, b = -4, 5
+    f = x -> x^4 + sin(x)
+    @test fibo(f, a, b) == (0.01, 0.02) 
+end
